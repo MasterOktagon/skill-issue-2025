3967b14aaa3b1bf2daaedfefb104677e889b9905
MasterOktagon <=>
2024-04-30 17:56:28 +0200

    ADD esp32.ino and Pins spec

---
+++ esp32/esp32.ino
@@ -0,0 +1,19 @@
+
+#include "shiftregister.h"
+
+void setup(){
+    Serial.begin(115200);
+    Serial.println("Serial init");
+    shiftregister::setup();
+    shiftregister::reset();
+    
+
+}
+
+void loop(){
+  shiftregister::set(SR_LED_L_GREEN, !shiftregister::get(SR_LED_L_GREEN));
+  delay(1000);
+
+}
+
+
