a25d233ac51a4734c5b49c6cc8b83f8f5b1e3a6a
MasterOktagon <userkontakt.hexagongames@gmail.com>
2024-05-21 13:16:52 +0200

    CHANGE: correct gitignore error

--- raspi-cpp/CMakeFiles/SkillIssue25-Raspi.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/SkillIssue25-Raspi.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/masteroktagon/Schreibtisch/NochKeinName/skill-issue-2025/raspi-cpp/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
--- raspi-cpp/CMakeFiles/SkillIssue25-Raspi.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SkillIssue25-Raspi.
-# This may be replaced when dependencies are built.
