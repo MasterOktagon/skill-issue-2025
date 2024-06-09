d96e654db01b9ad645420ba066b9b7f9061e80eb
MasterOktagon <userkontakt.hexagongames@gmail.com>
2024-06-09 11:58:34 +0200

    CHANGE: use gyroscope to imrpove the line following

--- esp32/linefollower.cpp
+++ esp32/linefollower.cpp
@@ -5,7 +5,7 @@
 #include "lightsensor.h"
 #include "linefollower.h"
 #include "motor.h"
-
+#include "gyro.h"
 
 using namespace std;
 
@@ -28,24 +28,27 @@ int16_t lf::follow(){
             #define timescale 1
         #endif
 
-        static int16_t last; // last computed difference
-        static int16_t bias; // bias (Integral)
-        int16_t mot_diff;    // output diff
+        static int16_t last;     // last computed difference
+        static int16_t bias = 0; // bias (Integral)
+        int16_t mot_diff;        // output diff
 
         // PID line follower
         #ifdef LF_USE_BACK
             int16_t diff_back = ls::back.left.value - ls::back.right.value;
         #endif
         int16_t diff = (ls::white.left.value - ls::white.center.value) - (ls::white.right.value - ls::white.center.value);
-        //int16_t diff_green = (ls::green.left.value - ls::red.left.value) - (ls::green.right.value - ls::red.right.value);  // difference to ignore green value
-        #define diff_green 0
+        int16_t diff_green = (ls::green.left.value - ls::red.left.value) - (ls::green.right.value - ls::red.right.value);  // difference to ignore green points
         int16_t diff_outer = ls::white.left_outer.value - ls::white.right_outer.value;
         mot_diff = int(float((diff + diff_green * 2) * 4 + diff_outer * diff_outer_factor) * mul * timescale);  // calculate inner to outer mult
         int16_t derivative = int(float(mot_diff - last / timescale));
         bias += mot_diff;
+        //#define bias 0
 
         #ifndef MOT_STBY
-            int16_t v = min(max(V_STD, int(V_STD - mot_diff * 0.5)), 50);  // scale base speed based on difference and gyro speed
+            int16_t v = V_STD;
+            if(abs(mot_diff) < 5){
+                v = 250;
+            }
 
             #ifdef LF_USE_BACK
                 float scale = 1 / ((abs(mot_diff) + 0.1) * 0.25);  // only use back LS if no difference on front is applied
@@ -57,16 +60,14 @@ int16_t lf::follow(){
                 #define vback_b 0
             #endif
 
-            /*// start motors
-            if (abs(gyro::XAngle) > 15) { // reduce correction on ramps
-                motor::fwd(motor::motor::A, v + (mot_diff - derivative * 2 + bias * 0.1 + vback_a) * 0.3);
-                motor::fwd(motor::motor::B, v - (mot_diff + derivative * 2 - bias * 0.1 - vback_b) * 0.3);
-            */
-            if(0){
-                
+            // start motors
+            if (abs(gyro::x) > 15) { // reduce correction on ramps
+                motor::fwd(motor::motor::A, v + (mot_diff - derivative * 2 + bias * 0.002 + vback_a) * 0.3);
+                motor::fwd(motor::motor::B, v - (mot_diff + derivative * 2 - bias * 0.002 - vback_b) * 0.3);
+
             } else {
-                motor::fwd(motor::motor::A, V_STD + mot_diff - derivative * 2 + bias * 0.01 + vback_a);
-                motor::fwd(motor::motor::B, V_STD - mot_diff + derivative * 2 - bias * 0.01 - vback_b);
+                motor::fwd(motor::motor::A, v + mot_diff - derivative * 2 + bias * 0.002 + vback_a);
+                motor::fwd(motor::motor::B, v - mot_diff + derivative * 2 - bias * 0.002 - vback_b);
             }
             last = mot_diff;
         #endif
