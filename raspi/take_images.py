ecd61228c88bd7a3119f1c24f1040eba3fac265a
Salkin-Niklas <nizdvfc@web.de>
2025-01-10 16:28:01 +0100

    take images with picam

---
+++ raspi/take_images.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import os
+import time
+
+
+path = "./recorded_images/"
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+for i in range(100):
+    ret, frame = cap.read()
+    if ret:
+        cv.imwrite(os.path.join(path, 'frame%d.jpg') % i, frame)
+
+cap.release()
