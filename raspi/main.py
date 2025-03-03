93f11b0eedcbeeb8f8c4571a04a6d86494a2b154
unknown <nizdvfc@web.de>
2025-03-03 21:06:18 +0100

    resolved missing function

--- raspi/main.py
+++ raspi/main.py
@@ -4,6 +4,8 @@ from picamera2 import Picamera2
 import os
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import backend as K
+import keras
 from PIL import Image
 
 camera = Picamera2()
@@ -23,6 +25,15 @@ last_corner_time = 0
 balls_active = False
 corners_active = False
 living_rescued = False
+
+@keras.saving.register_keras_serializable(package="custom_package", name="custom_mse")
+def custom_mse(y_true, y_pred):
+    mask = K.not_equal(K.sum(y_true, axis=1), 0.0)
+    y_true_custom = y_true[mask]
+    y_pred_custom = y_pred[mask]
+    mse = tf.keras.losses.MeanSquaredError()
+    result = mse(y_true_custom, y_pred_custom) # * 0.3
+    return result
 
 def i2c(id, tick):
     global pi
@@ -81,33 +92,38 @@ model = tf.keras.models.load_model("ball_detection_cuda.keras")
 camera_fov = 53.50  # richtig?
 TRUST = 0.5         # verändern?
 
-while  True:
-    print("wating")
-    #time.sleep(1)
-    if balls_active:
-        last_ball_time = time.time()
-        frame = np.array(camera.capture_array())
-        frame = frame.crop((0,150,640,66))
-        frame = frame.transpose(Image.FLIP_TOP_BOTTOM)
-        frame = np.expand_dims(frame/255, axis=0)
-        print(frame.shape)
-        yp_class, yp_box = model.predict(frame)
-        print("Model Prediction Class: ", yp_class)
-        print("Model Prediction Box: ", yp_box)
-        if(yp_class>TRUST):
-            angle = (yp_box[0][0]+yp_box[0][2]/2)/640 *camera_fov - camera_fov/2
-            print("Ball Angle", angle)
+
+def main():
+    while  True:
+        print("wating")
+        #time.sleep(1)
+        if balls_active:
+            last_ball_time = time.time()
+            frame = np.array(camera.capture_array())
+            frame = frame.crop((0,150,640,414))
+            frame = frame.transpose(Image.FLIP_TOP_BOTTOM)
+            frame = np.expand_dims(frame/255, axis=0)
+            print(frame.shape)
+            yp_class, yp_box = model.predict(frame)
+            print("Model Prediction Class: ", yp_class)
+            print("Model Prediction Box: ", yp_box)
+            if(yp_class>TRUST):
+                angle = (yp_box[0][0]+yp_box[0][2]/2)/640 *camera_fov - camera_fov/2
+                print("Ball Angle", angle)
+            else:
+                print("no Ball")
+        elif corners_active:
+            pass
         else:
-            print("no Ball")
-    elif corners_active:
-        pass
-    else:
-        pass
+            pass
 
 
 
-e.cancel()
+    e.cancel()
 
-pi.bsc_i2c(0) # Disable BSC peripheral
+    pi.bsc_i2c(0) # Disable BSC peripheral
 
-pi.stop()
+    pi.stop()
+
+if __name__ == "__main__":
+    main()
